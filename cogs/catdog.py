5a1128ca95420c010815b75d13569aee6d734758
mattelenz <matt.elenz@gmail.com>
2025-03-06 19:19:45 -0600

    cat -> catdog now with dog pics

--- cogs/catdog.py
+++ cogs/catdog.py
@@ -8,31 +8,32 @@ class Catdog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.cat_api_key = os.getenv('CAT_API_KEY')
+        self.dog_api_key = os.getenv('DOG_API_KEY')
 
     @app_commands.command(name="dog", description="Get a random dog picture!")
     async def cat(self, interaction: discord.Interaction):
         await interaction.response.defer()
 
         headers = {
-            'x-api-key': self.cat_api_key
+            'x-api-key': self.dog_api_key
         }
         async with aiohttp.ClientSession() as session:
             try:
-                async with session.get('https://api.thecatapi.com/v1/images/search', headers=headers) as response:
+                async with session.get('https://api.thedogapi.com/v1/images/search', headers=headers) as response:
                     if response.status != 200:
-                        await interaction.followup.send("Sorry, I couldn't fetch a cat pic right now. Please try again later.")
+                        await interaction.followup.send("Sorry, I couldn't fetch a dog pic right now. Please try again later.")
                         return
                     
                     data = await response.json()
                     if not data:
-                        await interaction.followup.send("Sorry, I couldn't find a cat picture.")
+                        await interaction.followup.send("Sorry, I couldn't find a dog picture.")
                         return
                     
-                    cat_image_url = data[0]['url']
-                    await interaction.followup.send(cat_image_url)
+                    dog_image_url = data[0]['url']
+                    await interaction.followup.send(dog_image_url)
             except aiohttp.ClientError as e:
-                print(f"Error fetching cat image: {e}")
-                await interaction.followup.send("Sorry, I couldn't fetch a cat pic right now. Please try again later.")
+                print(f"Error fetching dog image: {e}")
+                await interaction.followup.send("Sorry, I couldn't fetch a dog pic right now. Please try again later.")
 
     @app_commands.command(name="cat", description="Get a random cat picture!")
     async def cat(self, interaction: discord.Interaction):
